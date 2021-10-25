350da642664c6b9fad55259d97bcdd51ce4b9600
Juan Pablo Bedoya Vasco <bedoyavascojuanpablo@gmail.com>
2021-10-25 17:35:22 -0500

    se guardan estilos

---
+++ scripts/newArticle.js
@@ -0,0 +1,16 @@
+const URL = 'http://localhost:4001/articulos';
+const form = document.getElementById('form');
+
+let nombre = document.getElementById('nombre');
+let descripcion= document.getElementById('descripcion');
+let categoria = document.getElementById('categoria');
+const registrar = document.getElementById('registrar');
+
+form.addEventListener('submit', (e)=>{
+e.preventDefault();
+let nombre = document.getElementById('nombre').value;
+let descripcion= document.getElementById('descripcion').value;
+let categoria = document.getElementById('categoria').value;
+// console.log(nombre, descripcion, categoria);
+
+})
