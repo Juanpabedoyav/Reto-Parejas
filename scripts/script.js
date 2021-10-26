d6f840d463550e7ec68e0d504326c781da4f8362
Juan Pablo Bedoya Vasco <bedoyavascojuanpablo@gmail.com>
2021-10-25 19:31:31 -0500

    se guardan estilos

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+
+let btnCamisetas = document.getElementById('btnCamisetas');
+let btnJeans = document.getElementById('btnJeans');
+let url = 'http://localhost:4000/articulos'
+
+btnCamisetas.addEventListener('click', () =>{
+    //if articulos.categoria=btnCamisetas.value{
+
+    //}
+    //http://localhost:4000/articulos
+    obtenerArticulos('http://localhost:4000/articulos');
+});
+
+
+
+btnJeans.addEventListener('click', () =>{
+    obtenerArticulos('http://localhost:4000/articulos');
+    obtenerArticulos('');
+
+});
+
+
+/*btnarticulos.addEventListener('click' ,() =>{
+    
+    obtenerArticulos('')
+    });*/
+
+    const obtenerArticulos = async(url) => {
+        //querySelector() selecciona por varios metodos no solo por id.
+        let mostrarArticulos = document.querySelector('.grid-articulos');
+        mostrarArticulos.innerHTML ='';
+        const resp = await fetch(url);
+        const data = await resp.json();
+        data.forEach(articulos => {
+            const {nombre, Descripcion, categoria, talla, imagen} = articulos;
+            mostrarArticulos.innerHTML +=
+            `    <div class="col articulos">
+            <a href="#" class="enlace-detalle-articulos">
+                <div class="card bg-dark text-white gradiente">                
+                    <img src="${imagen}" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                            <h5 class="card-title body2Bold">${categoria}</h5>
+                            <p class="card-text body2Regular">${Descripcion}</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+            `
+        })
+        
+    }
+
+    obtenerArticulos('http://localhost:4000/articulos');
